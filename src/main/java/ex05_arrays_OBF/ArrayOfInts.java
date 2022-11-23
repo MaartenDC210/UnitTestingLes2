db86a9bd8ce584d9eff5b00572d267a7eb5c9ff3
mdecat <maarten.de.cat@sarens.com>
2022-11-23 19:46:13 +0100

    First commit

---
+++ src/main/java/ex05_arrays_OBF/ArrayOfInts.java
@@ -0,0 +1,28 @@
+package ex05_arrays_OBF;
+
+public class ArrayOfInts {
+
+    public int sumOfElementsInArray(int[] array) {
+        if (array == null) return 0;
+        int sum = 0;
+
+        for (int i : array) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public int sumOfEvenNumbers(int[] array) {
+        if (array == null) return 0;
+
+        int sum = 0;
+        for (int element : array) {
+            if (element % 2 == 0)
+                sum += element;
+
+        }
+
+        return sum;
+    }
+
+}
